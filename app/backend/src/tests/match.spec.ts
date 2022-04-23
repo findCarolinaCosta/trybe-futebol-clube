a52e552b57f94d3734e59322e1b06a58d7f0b56c
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-04-22 21:57:14 -0300

    Add tests for adding matches and editing match progress endpoints

--- app/backend/src/tests/match.spec.ts
+++ app/backend/src/tests/match.spec.ts
@@ -1,22 +1,27 @@
+import { ITeam } from "@interface/teamInterfaces";
 import "mocha";
 import * as sinon from "sinon";
 import * as chai from "chai";
 import chaiHttp = require("chai-http");
-import { IMatchTest } from "../interfaces/matchInterfaces";
+import { IMatch } from "../interfaces/matchInterfaces";
 
 import { app } from "../app";
 import MatchModel from "../database/models/match";
+import TeamModel from "../database/models/team";
 
 import { Response } from "superagent";
+import TeamService from "../services/team";
+
+const teamService = new TeamService(TeamModel);
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe("Testa rotas Matches", () => {
+describe("Testa rotas de partidas", () => {
   let chaiHttpResponse: Response;
   describe("caso de sucesso na requisição de todos as partidas da rota GET '/matches'", () => {
-    const MockResponseAll: IMatchTest[] = [
+    const MockResponseAll: IMatch[] = [
       {
         id: 1,
         homeTeam: "16",
@@ -47,7 +52,7 @@ describe("Testa rotas Matches", () => {
       },
     ];
 
-    const MockResToCompareAll: IMatchTest[] = [
+    const MockResToCompareAll: IMatch[] = [
       {
         id: 1,
         homeTeam: "16",
@@ -102,7 +107,7 @@ describe("Testa rotas Matches", () => {
   });
 
   describe("retorna somente partidas em andamento GET 'matches?inProgress=true' com query string", () => {
-    const MockResponseAll: IMatchTest[] = [
+    const MockResponseAll: IMatch[] = [
       {
         id: 41,
         homeTeam: "16",
@@ -133,7 +138,7 @@ describe("Testa rotas Matches", () => {
       },
     ];
 
-    const MockResToCompareAll: IMatchTest[] = [
+    const MockResToCompareAll: IMatch[] = [
       {
         id: 41,
         homeTeam: "16",
@@ -191,7 +196,7 @@ describe("Testa rotas Matches", () => {
   });
 
   describe("retorna somente partidas finalizadas GET 'matches?inProgress=false' com query string", () => {
-    const MockResponseAll: IMatchTest[] = [
+    const MockResponseAll: IMatch[] = [
       {
         id: 41,
         homeTeam: "16",
@@ -222,7 +227,7 @@ describe("Testa rotas Matches", () => {
       },
     ];
 
-    const MockResToCompareAll: IMatchTest[] = [
+    const MockResToCompareAll: IMatch[] = [
       {
         id: 41,
         homeTeam: "16",
@@ -276,6 +281,168 @@ describe("Testa rotas Matches", () => {
 
       expect(chaiHttpResponse).to.have.status(200);
       expect(chaiHttpResponse.body).to.be.deep.equals(MockResToCompareAll);
+    });
+  });
+
+  describe("cria partida com opção de inProgress como true", () => {
+    const MockResponse: IMatch = {
+      id: 1,
+      homeTeam: "16",
+      awayTeam: "8",
+      homeTeamGoals: "2",
+      awayTeamGoals: "2",
+      inProgress: true,
+    };
+
+    const MockResToCompare: IMatch = {
+      id: 1,
+      homeTeam: "16",
+      awayTeam: "8",
+      homeTeamGoals: "2",
+      awayTeamGoals: "2",
+      inProgress: true,
+    };
+
+    const arrayMockResTeam: ITeam[] = [
+      {
+        id: 8,
+        teamName: "Time 1",
+      },
+      {
+        id: 16,
+        teamName: "Time 2",
+      }
+    ];
+
+    // forma mais visível já que precisava "responder" duas vezes ...
+    const callbackTeam = sinon.stub(teamService, "findById");
+
+    callbackTeam.onCall(0).resolves(arrayMockResTeam[0] as ITeam);
+    callbackTeam.onCall(1).resolves(arrayMockResTeam[1] as ITeam);
+
+    before(async () => {
+      callbackTeam.onFirstCall();
+      callbackTeam.onSecondCall();
+      // ... outra forma era
+      // sinon.stub(teamService, "findById")
+      // .resolves(arrayMockResTeam[0] as ITeam) 1
+      // .resolves(arrayMockResTeam[1] as ITeam) 2
+      sinon.stub(MatchModel, "create").resolves(MockResponse as MatchModel);
+    });
+
+    after(() => {
+      (teamService.findById as sinon.SinonStub).restore();
+      (MatchModel.create as sinon.SinonStub).restore();
+    });
+
+    it("retorna partida criada com inProgress: true", async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post("/matches")
+        .send({
+          homeTeam: 16,
+          awayTeam: 8,
+          homeTeamGoals: 2,
+          awayTeamGoals: 2,
+        })
+        .then((res) => {
+          return res;
+        });
+
+      expect(chaiHttpResponse).to.have.status(201);
+      expect(chaiHttpResponse.body).to.be.deep.equals(MockResToCompare);
+    });
+  });
+
+  describe("atualiza progresso da partida colocando inProgress como false", () => {
+    const MockResponse: [number, IMatch[]] = [1, []]; // 1 linha afetada
+
+    before(async () => {
+      sinon
+        .stub(MatchModel, "update")
+        .resolves(MockResponse as [number, MatchModel[]]);
+    });
+
+    after(() => {
+      (MatchModel.update as sinon.SinonStub).restore();
+    });
+
+    it("retorna partida criada com inProgress: false", async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .patch("/matches/1/finish")
+        .then((res) => {
+          return res;
+        });
+
+      expect(chaiHttpResponse).to.have.status(200);
+      expect(chaiHttpResponse.body).to.be.deep.equals({ message: "OK" });
+    });
+  });
+
+  describe("erro ao tentar criar partidas com times iguais", () => {
+    const MockResponse = {};
+
+    before(async () => {
+      sinon.stub(MatchModel, "create").resolves(MockResponse as MatchModel);
+    });
+
+    after(() => {
+      (MatchModel.create as sinon.SinonStub).restore();
+    });
+
+    it("retorna erro ao tentar criar partida com times iguais", async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post("/matches")
+        .send({
+          homeTeam: 16,
+          awayTeam: 16,
+          homeTeamGoals: 2,
+          awayTeamGoals: 2,
+        })
+        .then((res) => {
+          return res;
+        });
+
+      expect(chaiHttpResponse).to.have.status(400);
+      expect(chaiHttpResponse.body).to.be.deep.equals({
+        message: "It is not possible to create a match with two equal teams",
+      });
+    });
+  });
+
+  describe("erro ao tentar criar partidas com times que não existe", () => {
+    const MockResponse = {};
+
+    before(async () => {
+      sinon.stub(teamService, "findById").resolves(null);
+      sinon.stub(MatchModel, "create").resolves(MockResponse as MatchModel);
+    });
+
+    after(() => {
+      (teamService.findById as sinon.SinonStub).restore();
+      (MatchModel.create as sinon.SinonStub).restore();
+    });
+
+    it("retorna erro ao tentar criar partida com times que não existe cadastrados no banco de dados", async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post("/matches")
+        .send({
+          homeTeam: 3000,
+          awayTeam: 8,
+          homeTeamGoals: 2,
+          awayTeamGoals: 2,
+        })
+        .then((res) => {
+          return res;
+        });
+
+      expect(chaiHttpResponse).to.have.status(404);
+      expect(chaiHttpResponse.body).to.be.deep.equals({
+        message: "There is no team with such id!",
+      });
     });
   });
 });
